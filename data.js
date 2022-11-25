39f5d7e5d7bb6bfd28086a01623cad54c3b72f20
Eugene Low <eugene.ws.low@gmail.com>
2022-11-25 09:25:00 +0800

    Add lazy load, generate images content from JSON

---
+++ data.js
@@ -0,0 +1,33 @@
+data = 
+[
+    {
+        "image":"photo1.jpeg",
+        "title":"Test, Digital",
+        "desc":"Test"
+    },
+    {
+        "image":"photo2.jpeg",
+        "title":"Test, Digital",
+        "desc":null
+    },
+    {
+        "image":"photo3.jpeg",
+        "title":"Test, Digital",
+        "desc":null
+    },
+    {
+        "image":"photo4.jpeg",
+        "title":"Test, Digital",
+        "desc":null
+    },
+    {
+        "image":"photo5.jpeg",
+        "title":"Test, Digital",
+        "desc":null
+    },
+    {
+        "image":"photo6.jpeg",
+        "title":"Test, Digital",
+        "desc":null
+    }
+];
--- main.js
+++ main.js
@@ -1,9 +1,56 @@
-const slides = document.querySelectorAll('.slide');
+const slider = document.querySelector('.slider');
 const next = document.querySelector('#next');
 const prev = document.querySelector('#prev');
-const auto = true;
+const auto = false;
 const intervalTime = 5000;
 let slideInterval;
+
+
+let style = document.createElement('style');
+document.getElementsByTagName('head')[0].appendChild(style);
+// Generate CSS
+function createClass(name,rules){
+  if(!(style.sheet||{}).insertRule) 
+      (style.styleSheet || style.sheet).addRule(name, rules);
+  else
+      style.sheet.insertRule(name+"{"+rules+"}",0);
+}
+
+
+for (let i = 0; i < data.length; i++) {
+  const element = data[i];
+
+  let content = document.createElement('div');
+  content.className = 'content';
+  let title = document.createElement('h1');
+  if (element.title !== null) {
+    let titleText = document.createTextNode(element.title);
+    title.appendChild(titleText);
+  }
+
+  let p = document.createElement('p');
+  if (element.desc !== null) {
+    let descText = document.createTextNode(element.desc);
+    p.appendChild(descText);
+  }
+  content.appendChild(title);
+  content.appendChild(p);
+
+  let slide = document.createElement('div');
+  if (i == 0) {
+    slide.className = 'slide load current ';
+  } else if (i == 1) {
+    slide.className = 'slide load';
+  } else {
+    slide.className = 'slide';
+  }
+  slide.appendChild(content);
+
+  slider.appendChild(slide);
+  createClass(`.slide:nth-child(${i+1}).load`,`background: url('./images/${element.image}') no-repeat center center/cover;`);  
+}
+
+const slides = document.querySelectorAll('.slide');
 
 const nextSlide = () => {
   // Get current class
@@ -14,6 +61,9 @@ const nextSlide = () => {
   if(current.nextElementSibling) {
     // Add current to next sibling
     current.nextElementSibling.classList.add('current');
+    if (current.nextElementSibling.nextElementSibling) {
+      current.nextElementSibling.nextElementSibling.classList.add('load');
+    }
   } else {
     // Add current to start
     slides[0].classList.add('current');
