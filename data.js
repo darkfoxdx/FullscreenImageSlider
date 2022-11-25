516bea9f9ae08f7656f7038953db0061b74c7ed0
Eugene Low <eugene.ws.low@gmail.com>
2022-11-25 14:09:14 +0800

    Add sample and overriding data

--- data.js
+++ data.js
@@ -1,36 +1,16 @@
-data = 
-{
-    "intro":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce cursus sapien id dapibus euismod. Integer porta lobortis ipsum eu tincidunt. Duis at ultricies ipsum. Pellentesque at est a ligula rhoncus vestibulum egestas vel purus. Nullam semper mollis nisi in lobortis. Integer congue vitae sem eget auctor. Quisque cursus metus at mi ultricies suscipit. Vestibulum hendrerit gravida augue. Curabitur elit odio, cursus ut varius ut, posuere sit amet nulla. Nunc in pulvinar nunc. Mauris id nulla gravida, rhoncus urna id, semper ex. Nunc varius non ligula vel iaculis. Etiam orci quam, euismod sit amet lacus in, porta cursus risus. Nunc ac hendrerit est.",
-    "content":[
-        {
-            "image":"photo1.jpeg",
-            "title":"Test, Digital",
-            "desc":"Test"
-        },
-        {
-            "image":"photo2.jpeg",
-            "title":"Test, Digital",
-            "desc":"Link is www.google.com"
-        },
-        {
-            "image":"photo3.jpeg",
-            "title":"Test, Digital",
-            "desc":null
-        },
-        {
-            "image":"photo4.jpeg",
-            "title":"Test, Digital",
-            "desc":null
-        },
-        {
-            "image":"photo5.jpeg",
-            "title":"Test, Digital",
-            "desc":null
-        },
-        {
-            "image":"photo6.jpeg",
-            "title":"Test, Digital",
-            "desc":null
-        }
-    ] 
-}
+// data = 
+// {
+//     "intro":"Insert intro here",
+//     "content":[
+//         {
+//             "image":"./images/photo1.jpeg",
+//             "title":"Test, Digital",
+//             "desc":"Test Link is www.google.com"
+//         },
+//         {
+//             "image":"./images/photo2.jpeg",
+//             "title":"Test, Digital",
+//             "desc":"Link<br/>NewLine"
+//         },
+//     ] 
+// }
